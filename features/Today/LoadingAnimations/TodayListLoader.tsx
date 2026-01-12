a06ff56d185654d33b9dc4d7f144bde43dba610b
Leah Steinberg <leahsteinberg1@gmail.com>
2026-01-11 22:40:37 -0800

    Reorganize loaders

--- features/Today/LoadingAnimations/TodayListLoader.tsx
+++ features/Today/LoadingAnimations/TodayListLoader.tsx
@@ -1,14 +1,7 @@
+import PulsingSpeechBubblesLoaderV2 from "@/components/LoadingAnimations/PulsingSpeechBubblesLoaderV2";
 import React from "react";
-import PulsingSpeechBubblesLoader from "./PulsingSpeechBubblesLoader";
 
-export enum LoaderType {
-    SHIMMER_SKELETON = "shimmer_skeleton",
-    PULSING_FLOWER = "pulsing_flower",
-    ANIMATED_DOTS = "animated_dots",
-    GLEAMING_CARD = "gleaming_card",
-    PULSING_SPEECH_BUBBLES = "pulsing_speech_bubbles",
-}
 
-export default function TodayListLoader({ selectedLoader }: { selectedLoader: LoaderType | undefined }): React.JSX.Element {
-    return <PulsingSpeechBubblesLoader />;
+export default function TodayListLoader(): React.JSX.Element {
+    return <PulsingSpeechBubblesLoaderV2 />;
 }
